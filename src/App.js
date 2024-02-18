ac3ecdf96c1a478a34c356c0e9629ee782be4180
Qzarov-work <qzarov@mail.ru>
2024-02-18 18:58:59 +0000

    Add site Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello world
-        </h1>
-      </header>
+    <div>
+      <Header/>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1>Hello world</h1>
+    //     <Button variant='primary'>Clict it now!</Button>
+    //   </header>
+    // </div>
   );
 }
 
